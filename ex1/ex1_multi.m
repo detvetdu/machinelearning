d50e3146b8ab91708d3a7667e9a5f04f11bc7f24
engle <anders.englesson@cag.se>
2018-05-18 12:34:41 +0200

    ex1 100% submit

--- ex1/ex1_multi.m
+++ ex1/ex1_multi.m
@@ -105,6 +105,7 @@ fprintf('\n');
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
 price = 0; % You should change this
+house = [1650 3]
 
 
 % ============================================================
