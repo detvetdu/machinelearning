3ed434c09cc5ffcc00e7dec274a87eedc1a7c930
engle <anders.englesson@cag.se>
2018-05-22 20:36:14 +0200

    ex3 costfunction 100% testcase

--- ex3/lrCostFunction.m
+++ ex3/lrCostFunction.m
@@ -36,10 +36,14 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
-
-
-
-
+% theta
+% X
+% y
+% lambda
+h = sigmoid(X*theta);
+theta_reg = [0 ; theta(2:size(theta)),:];
+J = (1/m) * ((-y' * log(h)) - ((1-y)' * log(1 - h))) + (lambda/(2*m)) * theta_reg' * theta_reg;
+grad = 1/m * X' * (h-y) + lambda/m * theta_reg;
 
 
 
